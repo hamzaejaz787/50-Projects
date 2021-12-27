02cdc9cb3f7edc61805c8f742a094f5d72eb1c2a
Hamza Ejaz <52288511+hamzaejaz787@users.noreply.github.com>
2021-12-27 19:03:09 +0500

    Add files via upload

--- Image Carousel/script.js
+++ Image Carousel/script.js
@@ -1,7 +1,33 @@
 function imageCarousel() {
   const highlight = document.querySelector(".carousel-highlight");
   const previews = document.querySelectorAll(".carousel-preview img");
+  const prevBtn = document.querySelector("#prevBtn");
+  const nextBtn = document.querySelector("#nextBtn");
 
+  //Image Slider
+  let counter = 0;
+
+  prevBtn.addEventListener("click", function () {
+    if (counter <= 0) return;
+    counter--;
+    const smallSrc = previews[counter].src;
+    const bigSrc = smallSrc.replace("-thumbnail", "");
+    highlight.src = bigSrc;
+    previews.forEach((preview) => preview.classList.remove("image-active"));
+    previews[counter].classList.add("image-active");
+  });
+
+  nextBtn.addEventListener("click", function () {
+    if (counter >= previews.length - 1) return;
+    counter++;
+    const smallSrc = previews[counter].src;
+    const bigSrc = smallSrc.replace("-thumbnail", "");
+    highlight.src = bigSrc;
+    previews.forEach((preview) => preview.classList.remove("image-active"));
+    previews[counter].classList.add("image-active");
+  });
+
+  //Image Carousel
   previews.forEach((preview) => {
     preview.addEventListener("click", function () {
       const smallSrc = this.src;
