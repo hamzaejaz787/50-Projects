84215d9f72953fb80509949cf53e66b031866c2c
Hamza Ejaz <52288511+hamzaejaz787@users.noreply.github.com>
2022-02-26 20:24:13 +0500

    Add files via upload

---
+++ Split Landing Page/script.js
@@ -0,0 +1,19 @@
+const left = document.querySelector(".playstation");
+const right = document.querySelector(".xbox");
+const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
