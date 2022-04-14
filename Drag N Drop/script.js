77b59436600ddeb506198cb95117c93d00e78d05
Hamza Ejaz <52288511+hamzaejaz787@users.noreply.github.com>
2022-04-15 02:13:29 +0500

    Add files via upload

--- Drag N Drop/script.js
+++ Drag N Drop/script.js
@@ -1,7 +1,5 @@
 function onDragStart(event) {
   event.dataTransfer.setData("text/plain", event.target.id);
-
-  event.currentTarget.style.backgroundColor = "yellow";
 }
 
 function onDragOver(event) {
